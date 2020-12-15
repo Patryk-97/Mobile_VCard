596c10730200a3b6cb0c05f2485fc95822cb13a3
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-12-15 14:27:36 +0100

    Added node-html-parser dependency

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,5 @@
 import { HttpService, Injectable } from '@nestjs/common';
+import { parse } from 'node-html-parser';
 
 @Injectable()
 export class AppService {
@@ -11,5 +12,41 @@ export class AppService {
     const htmlData = response.data;
 
     return htmlData;
+  }
+
+  getCardsFromHtmlData(htmlData: any): Event[] {
+    const root = parse(htmlData);
+    const activeClasses = root.querySelectorAll('.active');
+    let events: Event[] = [];
+    activeClasses.forEach(activeClass => {
+      if (activeClass.rawTagName === 'td') {
+        const activeHyperlink = activeClass.querySelector('.active');
+        if (activeHyperlink) {
+          let hyperlink = activeHyperlink.getAttribute('href');
+          if (hyperlink === 'javascript:void();')
+          {
+            hyperlink = '';
+          }
+          const innerBox = activeClass.querySelector('.InnerBox');
+          if (innerBox) {
+            const eventNameWrapper = (() => {
+              const p = innerBox.querySelector('p');
+              if (p) {
+                return p;
+              }
+              return innerBox.querySelector('div');
+            }) ();
+            if (eventNameWrapper) {
+              events.push({
+                day: parseInt(activeHyperlink.rawText),
+                name: eventNameWrapper.rawText,
+                hyperlink: hyperlink
+              });
+            }
+          }
+        }
+      }
+    });
+    return events;
   }
 }
