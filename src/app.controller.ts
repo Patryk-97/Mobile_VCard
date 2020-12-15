c585ffa92f94b4b87e5dfc73104a27da235c28db
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-12-15 14:20:12 +0100

    Implemented getting html content from panoramafirm website with proper name and localization parameters

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('name=:name&localization=:localization')
+  async getCards(@Param('name') name: string, @Param('localization') localization: string): Promise<any> {
+    return await this.appService.getCards(name, localization);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+
+  async getCards(name: string, localization: string): Promise<string> {
+    let query = 'https://panoramafirm.pl/szukaj?k=' + name;
+    query += '&l=' + localization;
+    const response = await this.httpService.get(query).toPromise();
+    const htmlData = response.data;
+
+    return htmlData;
   }
 }
