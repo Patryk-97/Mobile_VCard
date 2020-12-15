6ef19404981cc7eeabcb6bd79b73e81ac19c5408
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-12-15 14:44:18 +0100

    Implemented getting companies data from html

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,48 +5,26 @@ import { parse } from 'node-html-parser';
 export class AppService {
   constructor(private httpService: HttpService) {}
 
-  async getCards(name: string, localization: string): Promise<string> {
+  async getCards(name: string, localization: string): Promise<any> {
     let query = 'https://panoramafirm.pl/szukaj?k=' + name;
     query += '&l=' + localization;
     const response = await this.httpService.get(query).toPromise();
     const htmlData = response.data;
-
-    return htmlData;
+    const cards = this.getCardsFromHtmlData(htmlData);
+    return cards;
   }
 
   getCardsFromHtmlData(htmlData: any): Event[] {
     const root = parse(htmlData);
-    const activeClasses = root.querySelectorAll('.active');
-    let events: Event[] = [];
-    activeClasses.forEach(activeClass => {
-      if (activeClass.rawTagName === 'td') {
-        const activeHyperlink = activeClass.querySelector('.active');
-        if (activeHyperlink) {
-          let hyperlink = activeHyperlink.getAttribute('href');
-          if (hyperlink === 'javascript:void();')
-          {
-            hyperlink = '';
-          }
-          const innerBox = activeClass.querySelector('.InnerBox');
-          if (innerBox) {
-            const eventNameWrapper = (() => {
-              const p = innerBox.querySelector('p');
-              if (p) {
-                return p;
-              }
-              return innerBox.querySelector('div');
-            }) ();
-            if (eventNameWrapper) {
-              events.push({
-                day: parseInt(activeHyperlink.rawText),
-                name: eventNameWrapper.rawText,
-                hyperlink: hyperlink
-              });
-            }
-          }
-        }
+    const scripts = root.querySelectorAll('script');
+
+    let arr = [];
+    scripts.forEach(script => {
+      const type = script.getAttribute('type');
+      if (type === 'application/ld+json') {
+        arr.push(script.rawText);
       }
     });
-    return events;
+    return arr;
   }
 }
